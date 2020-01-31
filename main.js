2d41949c92a7d728aea6b03944c8416c0afb390e
lo7656 <vladleonchik@mail.ru>
2020-01-31 16:15:17 +0300

    test

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function maker(json){
-    switch(location.pathname){
-        case "/fruit":
-            json = json.Фрукты
-            break
+    // switch(location.pathname){
+    //     case "/fruit":
+    //         json = json.Фрукты
+    //         break
         
-        case "/vegetables":
-            json = json.Овощи
-            break
+    //     case "/vegetables":
+    //         json = json.Овощи
+    //         break
 
-        case "/herbs":
-            json = json.Травы
-            break
-    }
+    //     case "/herbs":
+    //         json = json.Травы
+    //         break
+    // }
     console.log(json)
     json.forEach(element => {
         let card = `<a class="localpagelist2__block" href="#"><span class="localpic localpagelist2__block-pic localpic--small"><img src="${element.image}" alt="" title="" /></span><span class="localpagelist2__block-text">${element.name}</span></a>`
@@ -22,5 +22,5 @@ function maker(json){
 
 fetch('https://raw.githubusercontent.com/lo7656/storage/master/data.json', { method: 'GET' })
     .then(response => response.json())
-    .then(body => maker(body))
+    .then(body => maker(body.Фрукты))
     .catch(error => console.log(error))
