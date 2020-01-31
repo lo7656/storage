552b2c257f199408741f77d4010416505f1e0239
lo7656 <vladleonchik@mail.ru>
2020-01-31 15:57:37 +0300

    update changing category

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 function maker(json){
+    switch(location.pathname){
+        case "/fruit":
+            json = json.Фрукты
+            break
+        
+        case "/vegetables":
+            json = json.Овощи
+            break
+
+        case "/herbs":
+            json = json.Травы
+            break
+    }
     console.log(json)
     json.forEach(element => {
         let card = `<a class="localpagelist2__block" href="#"><span class="localpic localpagelist2__block-pic localpic--small"><img src="${element.image}" alt="" title="" /></span><span class="localpagelist2__block-text">${element.name}</span></a>`
@@ -9,5 +22,5 @@ function maker(json){
 
 fetch('https://raw.githubusercontent.com/lo7656/storage/master/data.json', { method: 'GET' })
     .then(response => response.json())
-    .then(body => maker(body.location.pathname))
+    .then(body => maker(body))
     .catch(error => console.log(error))
