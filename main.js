dd623824209f6e9ae152f3c8d8e495549d888fd9
lo7656 <vladleonchik@mail.ru>
2020-01-31 13:57:35 +0300

    add main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+function maker(json){
+    console.log(json)
+    json.forEach(element => {
+        let card = `<a class="localpagelist2__block" href="#"><span class="localpic localpagelist2__block-pic localpic--small"><img src="${element.image}" alt="" title="" /></span><span class="localpagelist2__block-text">${element.name}</span></a>`
+        document.querySelector('.localpagelist2').innerHTML += card;
+        console.log(card)
+    });
+}
+
+fetch('https://raw.githubusercontent.com/lo7656/storage/master/data.json', { method: 'GET' })
+    .then(response => response.json())
+    .then(body => maker(body.Овощи))
+    .catch(error => console.log(error))
