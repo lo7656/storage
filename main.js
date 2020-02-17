6046418ab401370fba544cf22a8baa23457a16ad
lo7656 <vladleonchik@mail.ru>
2020-02-17 15:25:28 +0300

    correct the mistake

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function maker(json) {
             break;
 
         case "/sausages":
-            json = json.Мясо
+            json = json.Мясныеизделия
             break;
     }
     console.log(json)
