df1dda0f566727e06ab38ca6695419a928ab6054
lo7656 <vladleonchik@mail.ru>
2020-04-21 01:06:08 +0300

    test

--- postMail.php
+++ postMail.php
@@ -1,14 +1,16 @@
 <?php
-$fio = $_POST['name'];
+$name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = 'dushez@yandex.ru';
-$fio = htmlspecialchars($fio);
+$name = htmlspecialchars($name);
 $phone = htmlspecialchars($phone);
 $email = htmlspecialchars($email);
-$fio = urldecode($fio);
+$name = urldecode($name);
 $phone = urldecode($phone);
 $email = urldecode($email);
-$fio = trim($fio);
+$name = trim($name);
 $phone = trim($phone);
 $email = trim($email);
+
+mail("vladleonchik@mail.ru", "Заказ с сайта", "Имя:".$name.". E-mail: ".$email ,"From: Владислав \r\n")
 ?>
