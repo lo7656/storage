b545cbd70350bf69cd7788701d81bd71620eb4e8
lo7656 <vladleonchik@mail.ru>
2020-04-22 09:37:09 +0300

    change url

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "viliton.by/t/postmail.php", //Change
+			url: "postmail.php", //Change
 			data: th.serialize()
 		}).done(function() {
 			alert("Thank you!");
