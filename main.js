461e43e97cea85fc842fddcfaad5ad8f74eef48c
lo7656 <vladleonchik@mail.ru>
2020-04-22 13:04:15 +0300

    correct

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 
 $(document).ready(function () {
     $('.trigger').on('click', function () {
-        let mas = document.querySelector('.localpagelist2__block-pic');
+        let mas = document.querySelectorAll('.localpagelist2__block-pic');
 
         mas.forEach(element => {
             element.style.position='inherit'
